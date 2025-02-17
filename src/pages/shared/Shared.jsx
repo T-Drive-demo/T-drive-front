e26d3717b287532b12e734fadcea2c0943c3e38c
sunkyoung <yskkjdh@gmail.com>
2025-02-17 14:27:27 +0900

    design : Shared 페이지 디자인

--- src/pages/shared/Shared.jsx
+++ src/pages/shared/Shared.jsx
@@ -1,10 +1,24 @@
 import { SharedContainer } from "styles/shared/shared.style";
 import PageHeader from "components/common/PageHeader";
+import { Suspense, useState } from "react";
+import FilesList from "components/common/FilesList";
 
 const Shared = () => {
+  // 추후 공유했던 파일을 가져오도록 구현할 예정
+  const [sharedFiles, setSharedFiles] = useState("");
+
   return (
     <SharedContainer>
       <PageHeader pageTitle={"Shared with me"} />
+      <Suspense>
+        <FilesList
+          data={sharedFiles}
+          page="shared"
+          imagePath={"./assets/img/shared.svg"}
+          text1={"No shared files"}
+          text2={"Find and manage all the files shared with you in one place."}
+        />
+      </Suspense>
     </SharedContainer>
   );
 };
